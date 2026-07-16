ff693dc4b6b232a0d976ee6d0a457a9c7ad74696
negativeiq <nathcyz@gmail.com>
2026-07-16 19:37:55 +0800

    2

--- app.cpp
+++ app.cpp
@@ -4,26 +4,21 @@
 #include    <fstream>
 #include    <string>
 
-#include    <sstream>
-
 #include	"List.h"
 #include    "LibStudent.h"
 #include    "LibBook.h"
 
-
 using namespace std;
 
-string[] splitString(string, int)
-
-bool ReadFile(string, List *);
-bool DeleteRecord(List *, char *);
-bool Display(List, int, int);
-bool InsertBook(string, List *);
-bool SearchStudent(List *, char *id, LibStudent &);
-bool computeAndDisplayStatistics(List *);
-bool printStuWithSameBook(List *, char *);
-bool displayWarnedStudent(List *, List *, List *);
-int menu();
+bool ReadFile(string, List);					// NAT
+bool DeleteRecord(List, char);					//SAM
+bool Display(List, int, int);					//NAT
+bool InsertBook(string, List);					//SAM
+bool SearchStudent(List, charid, LibStudent&);	//NAT
+bool computeAndDisplayStatistics(List);			//MARCUS
+bool printStuWithSameBook(List, char);			//SAM
+bool displayWarnedStudent(List, List, List);	//MARCUS
+int menu();										//NAT
 
 
 int main() {
@@ -37,13 +32,38 @@ int main() {
 
 bool ReadFile(string filename, List* list) {
 
-	infile studentFile(filename);
+	ifstream studentFile(filename);
 
 	LibStudent student;
-	string 
+	string line;
+	int count, pos;
+	bool checks[4] = {false, false, false, false};
 
-	while (!studentFile.eof()) {
-		studentFile 
+	while (getline(studentFile, line)) {
+		pos = line.find("=");
+		if (pos == string::npos) continue;
+
+		switch (line.substr(0, pos-1)) {
+		case "Student Id":
+
+			checks[0] = true;
+			break;
+		case "Name":
+
+			checks[1] = true;
+			break;
+		case "course":
+
+			checks[2] = true;
+			break;
+		case "Phone Number":
+
+			checks[2] = true;
+			break;
+		default:
+			cout << "Error parsing " << filename << ": bad attribute identifier";
+		}
+
 	}
 
 }
